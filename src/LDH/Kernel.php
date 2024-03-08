220def43eb5abc1c4acab4ca6abda50a38efb566
lukehayes <lukepurplehayes@gmail.com>
2024-03-08 09:12:39 +0000

    Added middleware array to LDH\Kernel class.

--- src/LDH/Kernel.php
+++ src/LDH/Kernel.php
@@ -6,11 +6,40 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Kernel
 {
+    /** @var middleware[] */
+    private $middleware = [];
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Start the application process.
+     *
+     * @param Reqeust $request.
+     *
+     * @return Response
+     */
     public function handle(Request $request): Response
     {
+        $this->runMiddleware($request);
+
         $content = "<p>Loaded</p>";
         $response = new Response($content);
 
         return $response;
+    }
+
+    /**
+     * Run every instance of middleware that has been defined.
+     *
+     * @param Reqeust $request.
+     */
+    public function runMiddleware(Request $request)
+    {
+        foreach($this->middleware as $middleware)
+        {
+            $middleware->next($request);
+        }
     }
 }
